9bf78f2ad47acd377191e63ebe34f2c4a6447d37
muroyyy <amirul.faiz01@gmail.com>
2025-12-18 19:09:40 +0800

    update ec2 instance type to t3.small

--- infra/modules/rds/main.tf
+++ infra/modules/rds/main.tf
@@ -57,7 +57,7 @@ resource "aws_db_instance" "main" {
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
   
-  backup_retention_period = 7
+  backup_retention_period = 0
   backup_window          = "03:00-04:00"
   maintenance_window     = "mon:04:00-mon:05:00"
   
--- infra/variables.tf
+++ infra/variables.tf
@@ -37,7 +37,7 @@ variable "key_pair_name" {
 variable "instance_type" {
   description = "EC2 instance type"
   type        = string
-  default     = "t3.medium"
+  default     = "t3.small"
 }
 
 variable "db_instance_class" {
