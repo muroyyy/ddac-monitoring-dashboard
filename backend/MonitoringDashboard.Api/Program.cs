d9b752e46e6949109fe7a492f66824959d3247cc
muroyyy <amirul.faiz01@gmail.com>
2025-12-18 19:51:28 +0800

    trigger backend push and updated backend CI/CD file because EC2_INSTANCE_ID was set just as a placeholder initially, updated it as a env variable

--- backend/MonitoringDashboard.Api/Program.cs
+++ backend/MonitoringDashboard.Api/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configure CORS for frontend
+// Configure CORS for frontendd
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
